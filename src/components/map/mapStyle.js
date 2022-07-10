f6ed55eb0b54af5003447b80c63a463a43c63008
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-07-10 15:27:03 -0500

    Added map (svg - the ids of each strock is the code for the State) and map styles

---
+++ src/components/map/mapStyle.js
@@ -0,0 +1,9 @@
+export const map = {
+    boxSizing: 'border-box',
+    gridRow: '2',
+    gridColumn:'1/3',
+    height: '75%',
+    width: '75%',
+    marginTop: '2.8rem',
+    marginLeft: '5rem'
+}
