4b939313ae3b7fa3349a847e565707825636bf7a
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2022-08-23 12:39:48 -0500

    added map to app file, fix map, change form, make more responsive

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,12 +1,26 @@
 import { fonts, colors} from "./components/fonts_colors"
 
+// 10 MOST COMMON SCREEN SIZES
+// 5	2560×1440	34,152(7.41%) 1
+// 1	1920×1080	104,190(22.62%) 2
+// 9	1792×1120	9,494(2.06%) 3
+// 6	1680×1050	19,730(4.28%) 4
+// 10	1600×900	8,630(1.87%) 5
+// 4	1536×864	39,606(8.60%) 6
+// 3	1440×900	44,003(9.55%) 7
+// 2	1366×768	51,580(11.20%) 8
+// 8	1280×800	10,719(2.33%) 9
+// 7	1280×720	16,364(3.55%) 10
+
+
+
+
 
 // Made Grid for all elements in the app
 export const appStyle = {
     boxSizing: 'border-box',
     background: colors.white,
     height: '100vh',
-    with: '100%',
     backgroundSize: '100% 100%',
     overflowX: 'hidden',
     borderLeft: 'black solid 1px', // Decorative
@@ -14,7 +28,8 @@ export const appStyle = {
     margin: '-0.8rem 4rem',
     padding: '0rem 5rem',
     display: 'grid',
-    gridTemplateColums: '40% 40% 20%',
-    gridTemplateRows: '13% 65% auto',
+    gridTemplateColumns: '35% 35% 30%',
+    gridTemplateRows: '13% 65% fit-content',
     gap: '2rem'
 }
+
